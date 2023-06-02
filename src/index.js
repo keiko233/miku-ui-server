a6b4e9aa8d2a3bfcc8c8dd3368486a6e67340acb
keiko233 <keiko23333@gmail.com>
2023-06-02 19:37:58 +0800

    Initial Code

---
+++ src/index.js
@@ -0,0 +1,48 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+const regexInfo = (text) => {
+  return {
+    device: text.match(/<div[^>]*>([^<]*)\s*\(([^)]+)\)/)[2].trim(),
+    name: text.match(/<div[^>]*>([^<]*)\s*\(([^)]+)\)/)[1].trim(),
+    version: text.match(/Miku UI [A-Za-z]+\s+v(\d+\.\d+\.\d+)/)[1],
+    android: text.match(/Android:\s*(\d+)\s*\([A-Za-z]\)/)[1],
+    status: text.match(/Status: ([^<]*)/)[1].trim(),
+    selinux: text.match(/SELinux:([^<]*)/)[1].trim(),
+    kernelsu: text.match(/KernelSU:\s+(\d+)/)[1].trim(),
+    data: text.match(/Updated: ([^<]*)/)[1].trim(),
+    sourcforge_url: text.match(/<a href="(https:\/\/sourceforge\.net\/[^"]+)"/)[1],
+    changelog: (text.match(/Changelog:(.*?)<br>\s*<br>/s) || [, ''])[1].trim().split('<br>').map(line => line.replace(/^- /, '').trim()).filter(line => line !== ''),
+    note: (text.match(/Notes:(.*?)<br>\s*<br>/s) || [, ''])[1].trim().split('<br>').map(line => line.replace(/^-/, '').trim()).filter(line => line !== '')
+  };
+};
+
+puppeteer.launch({
+  // slowMo: 500,
+  devtools: true
+}).then(async browser => {
+  const page = await browser.newPage();
+  await page.goto('https://t.me/s/mikuuirelease');
+  const result = await page.evaluate(async () => {
+    const content = document.getElementsByClassName('tgme_widget_message_text');
+    console.log(content);
+    return [...content].map(div => div.outerHTML);
+  });
+
+  const jsonData = [];
+
+  for (let i = result.length; i > 0; i--) {
+    if (result[i] && /\([A-Za-z]+\)/.test(result[i])) jsonData.push(regexInfo(result[i]));
+  }
+
+  console.log("jsonData:" + jsonData);
+
+  fs.writeFile('device.json', JSON.stringify(jsonData), (err) => {
+    if (err) {
+      throw err;
+    }
+    console.log("JSON data is saved.");
+  });
+
+  await browser.close();
+});
