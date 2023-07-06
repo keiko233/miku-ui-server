d0e53373f909fe36615eda426b854e4429436163
keiko233 <keiko23333@gmail.com>
2023-07-07 01:03:54 +0800

    feat: add typescript support & add database utils with sqlite3

---
+++ src/utils/database.ts
@@ -0,0 +1,90 @@
+import sqlite3 from 'sqlite3';
+import * as config from '../config.json';
+
+class Database {
+  private db: sqlite3.Database;
+
+  constructor() {
+    this.db = new sqlite3.Database(config.data_path + config.database_file_name);
+    this.createTable();
+  }
+
+  private createTable() {
+    this.db.run(`CREATE TABLE IF NOT EXISTS devices (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      device TEXT,
+      name TEXT,
+      version TEXT,
+      android TEXT,
+      status TEXT,
+      selinux TEXT,
+      kernelsu TEXT,
+      data TEXT,
+      sourcforge_url TEXT,
+      changelog TEXT,
+      note TEXT
+    )`);
+  }
+
+  public insertData(item: any) {
+    const selectStmt = this.db.prepare('SELECT * FROM devices WHERE device = ? AND version = ?');
+    selectStmt.get(item.device, item.version, (err: any, row: any) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      if (row) {
+        console.log('Data already exists in the database. Skipped insertion.');
+      } else {
+        const insertStmt = this.db.prepare(`INSERT INTO devices (
+          device, name, version, android, status, selinux, kernelsu, data, sourcforge_url, changelog, note
+        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`);
+
+        insertStmt.run(
+          item.device,
+          item.name,
+          item.version,
+          item.android,
+          item.status,
+          item.selinux,
+          item.kernelsu,
+          item.data,
+          item.sourcforge_url,
+          JSON.stringify(item.changelog),
+          JSON.stringify(item.note)
+        );
+
+        insertStmt.finalize();
+        console.log('Data inserted into the database.');
+      }
+    });
+    selectStmt.finalize();
+  }
+
+  public getData(device?: string): Promise<any[]> {
+    return new Promise((resolve, reject) => {
+      let query = 'SELECT * FROM devices';
+      let params: any[] = [];
+
+      if (device) {
+        query += ' WHERE device = ?';
+        params.push(device);
+      }
+
+      this.db.all(query, params, (err, rows) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(rows);
+        }
+      });
+    });
+  }
+
+  public close() {
+    this.db.close();
+  }
+}
+
+export default new Database();
