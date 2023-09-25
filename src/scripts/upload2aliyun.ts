f62cfa5eac82da0ce9dbca6c4bded7834c509683
keiko233 <keiko23333@gmail.com>
2023-09-25 15:18:17 +0800

    feat: upload2aliyun support floder input

--- src/scripts/upload2aliyun.ts
+++ src/scripts/upload2aliyun.ts
@@ -2,6 +2,8 @@ import { uploadFile } from "../utils/aliyun";
 
 const path = process.argv[2];
 
-uploadFile([path], "test", (result: any) => {
+const floder = process.argv[3];
+
+uploadFile([path], floder, (result: any) => {
   console.log(result);
 });
